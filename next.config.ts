e36d4182b83f748dc4c875f65ba374ca8620c78a
Tanush Maurya <64687247+Tanushmaurya018@users.noreply.github.com>
2025-10-15 01:52:55 +0530

    added backend api

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*", // Proxy to backend
+        destination: "https://fairsplit-api.onrender.com/api/:path*", // Proxy to backend
       },
     ];
   },
